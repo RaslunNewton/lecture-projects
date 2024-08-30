b6a6fa0b733612f0cf7424b4e714ced67a4a10ef
RaslunNewton <rasmus.lundgren@edu.newton.se>
2024-08-30 10:32:17 +0200

    1

---
+++ lecture_project_24-08-30/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            sum += i;
+            Console.WriteLine($"i: {i} \nsum = {sum}");
+
+            if (sum == 3)
+            {
+                Console.WriteLine("We have now the magical sum of 3");
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
